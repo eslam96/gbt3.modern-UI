c65978c9d2261063c6c7c720caf1daf36e044f34
FAWRY\Eslam.Mamdouh <eslam.mamdouh@fawry.com>
2023-02-13 15:07:28 +0200

    updating navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import styles from "./navbar.module.scss";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 
-const MenuLinks = () => {
+const Links = () => {
   return (
     <>
       <p>
         <a href="#home">Home</a>
       </p>
       <p>
-        <a href="#wgbt3">What is GBT3</a>
+        <a href="#wgpt3">What is GPT3</a>
       </p>
       <p>
         <a href="#possibility">OpenAI</a>
@@ -30,20 +30,22 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className={styles.gbt3__navbar}>
-      <div className={styles["gbt3__navbar-links"]}>
-        <div className={styles["gbt3__navbar-links_logo"]}>
+    <div className={styles["gpt3__navbar"]}>
+      <div className={styles["gpt3__navbar-links"]}>
+        <div className={styles["gpt3__navbar-links_logo"]}>
           <img src={logo} alt="logo" />
         </div>
-        <div className={styles["gbt3__navbar-links_container"]}>
-          <MenuLinks />
+        <div className={styles["gpt3__navbar-links_container"]}>
+          <Links />
         </div>
       </div>
-      <div className={styles["gbt3__navbar-sign"]}>
+      <div className={styles["gpt3__navbar-sign"]}>
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
-      <div className={styles["gbt3__navbar-menu"]}>
+
+      {/* mobile view links menu */}
+      <div className={styles["gpt3__navbar-menu"]}>
         {toggleMenu ? (
           <RiCloseLine
             color="#fff"
@@ -63,11 +65,11 @@ const Navbar = () => {
         )}
         {toggleMenu && (
           <div
-            className={styles["gbt3__navbar-menu_container scale-up-center"]}
+            className={`${styles["gpt3__navbar-menu_container"]} scale-up-center`}
           >
-            <div className={styles["gbt3__navbar-menu_container-links"]}>
-              <MenuLinks />
-              <div className={styles["gbt3__navbar-menu_container-links-sign"]}>
+            <div className={styles["gpt3__navbar-menu_container-links"]}>
+              <Links />
+              <div className={styles["gpt3__navbar-menu_container-links-sign"]}>
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
               </div>
